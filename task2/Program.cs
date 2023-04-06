595c4b32cfe6920904e4e80f744dda6a66637f8b
Zeradeil <ivanosh0@bk.ru>
2023-04-06 15:31:07 +0700

    решено 2

--- task2/Program.cs
+++ task2/Program.cs
@@ -10,5 +10,11 @@ int ReadNum(string massage)
 }
 
 int number = ReadNum("Введите число");
+int sum = 0;
 
-
+while (number > 0)
+{
+    sum = sum + (number % 10);
+    number = number / 10;
+}
+System.Console.WriteLine(sum);
